bbcd07e6f32a5d378e41cb702a46521e4120b593
Lucas <keyruu@web.de>
2026-02-25 21:07:14 +0100

    update stuff

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,11 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-  // Type-check frontmatter using a schema
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
     draft: z.boolean().default(false),
     pubDate: z.coerce.date().optional(),
     updatedDate: z.coerce.date().optional(),
